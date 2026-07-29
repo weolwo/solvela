5f81e8a2526dbe0ef1c0d18f5b03f1e12080f0a8
admin <admin@qq.com>
2026-07-29 08:38:40 +0800

    [MOD] 删除 objenesis 排除pom，高版本 redisson 已适配，他会自己解决版本问题

--- smart-admin-api-java17-springboot3/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/login/domain/LoginResultVO.java
+++ smart-admin-api-java17-springboot3/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/login/domain/LoginResultVO.java
@@ -3,7 +3,6 @@ package net.lab1024.sa.admin.module.system.login.domain;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import net.lab1024.sa.admin.module.system.menu.domain.vo.MenuVO;
-import net.minidev.json.annotate.JsonIgnore;
 
 import java.time.LocalDateTime;
 import java.util.List;
